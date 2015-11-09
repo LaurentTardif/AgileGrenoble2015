6eb75b8f9e2785d9ad6ea407e1702326076f7449
Franck Rageade <franck.rageade@persistent.com>
2015-11-09 10:42:08 +0100

    Add files to display a tag cloud

---
+++ FrontEnd/tagCloud/cloud_generator.php
@@ -0,0 +1,27 @@
+﻿<?php
+/** genere le code HTML du nuage */
+function getCloudHTML( ) {
+	global $TAGS_ARRAY;
+	global $NB_STYLES, $MAX_WEIGHT, $MIN_WEIGHT;
+	
+	// Calcul du facteur d'echelle
+	$scale = round ( $NB_STYLES / ( $MAX_WEIGHT - $MIN_WEIGHT + 1 ) );
+	
+	// Generation du code
+	$retour = "<div class=\"tags_box\">";
+	
+	if ( count ( $TAGS_ARRAY ) ) {
+		foreach ( $TAGS_ARRAY as $tag_element) {
+			// Determination de la taille du tag
+			$tag_size = round ( $tag_element["weight"] * $scale);
+			$retour .= "\n\t<a class=\"cloud" . $tag_size . "\">";
+			$retour .= htmlentities( $tag_element["tag"] );
+			$retour .= "</a>";
+		}
+	}
+	
+	$retour .= "\n</div>";
+	
+	return $retour;
+}
+?>
