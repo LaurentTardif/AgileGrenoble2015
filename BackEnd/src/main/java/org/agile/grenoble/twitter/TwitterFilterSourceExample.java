d47b248fe8802571d541281de40de61b309319a7
Laurent Tardif <Laurent.tardif2010@gmail.com>
2015-11-10 15:58:51 +0100

    parse json and add track term on twitter

--- BackEnd/src/main/java/org/agile/grenoble/twitter/TwitterFilterSourceExample.java
+++ BackEnd/src/main/java/org/agile/grenoble/twitter/TwitterFilterSourceExample.java
@@ -21,6 +21,8 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.agile.grenoble.twitter.JSONParseFlatMap;
 import org.apache.flink.util.Collector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This is an example how to use TwitterFilterSource. Before executing the
@@ -28,22 +30,61 @@ import org.apache.flink.util.Collector;
  * resource folder. The access keys can be found in your twitter account.
  */
 public class TwitterFilterSourceExample {
-
+    private static final Logger LOG = LoggerFactory.getLogger(TwitterFilterSourceExample.class);
 	/**
 	 * path to the twitter properties
 	 */
-	private static final String PATH_TO_AUTH_FILE = "/twitter.properties";
+	//private static final String PATH_TO_AUTH_FILE = "/twitter.properties";
 
 	public static void main(String[] args) {
+		if (!parseParameters(args)) {
+			System.out.println("Arguments fail!");
+			System.exit (1);
+		};
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment
 				.getExecutionEnvironment();
 
-		TwitterFilterSource twitterSource = new TwitterFilterSource(
-				TwitterFilterSourceExample.class.getResource(PATH_TO_AUTH_FILE)
-						.getFile());
 
-		twitterSource.trackTerm("obama");
-		twitterSource.filterLanguage("en");
+        /* example de tweet in json
+
+        {"created_at":"Tue Nov 10 14:46:15 +0000 2015",
+        "id":664091704534933504,"
+        id_str":"664091704534933504",
+        "text":"RT @SofteamCadextan: Fin de journ\u00e9e avec une formation d'initiation \u00e0 l'Agile - #Agilit\u00e9 https:\/\/t.co\/2euIg5iS8H",
+        "source":"\u003ca href=\"http:\/\/twitter.com\"
+        rel=\"nofollow\"\u003eTwitter Web Client\u003c\/a\u003e","
+        truncated":false,"in_reply_to_status_id":null,
+        "in_reply_to_status_id_str":null,"in_reply_to_user_id":null,
+        "in_reply_to_user_id_str":null,"in_reply_to_screen_name":null,
+        "user":{
+            "id":216406867,"id_str":"216406867","name":"Laurent Fourmy","screen_name":"LaurentFourmy","location":"Sophia Antipolis","url":"http:\/\/www.softeam.fr","description":null,"protected":false,"verified":false,"followers_count":46,"friends_count":76,"listed_count":9,"favourites_count":4,"statuses_count":93,
+            "created_at":"Tue Nov 16 16:55:03 +0000 2010","utc_offset":null,"time_zone":null,"geo_enabled":false,
+            "lang":"fr","contributors_enabled":false,"is_translator":false,"profile_background_color":"C0DEED",
+            "profile_background_image_url":"http:\/\/abs.twimg.com\/images\/themes\/theme1\/bg.png",
+            "profile_background_image_url_https":"https:\/\/abs.twimg.com\/images\/themes\/theme1\/bg.png",
+            "profile_background_tile":false,"profile_link_color":"0084B4","profile_sidebar_border_color":"C0DEED",
+            "profile_sidebar_fill_color":"DDEEF6","profile_text_color":"333333","profile_use_background_image":true,
+            "profile_image_url":"http:\/\/pbs.twimg.com\/profile_images\/491945647994462209\/rVdUOy_V_normal.jpeg",
+            "profile_image_url_https":"https:\/\/pbs.twimg.com\/profile_images\/491945647994462209\/rVdUOy_V_normal.jpeg",
+            "profile_banner_url":"https:\/\/pbs.twimg.com\/profile_banners\/216406867\/1445534192","default_profile":true,
+            "default_profile_image":false,"following":null,"follow_request_sent":null,"notifications":null},
+          "geo":null,
+          "coordinates":null,"place":null,"contributors":null,
+          "retweeted_status":... }
+
+         */
+
+
+
+
+
+		/*TwitterFilterSource twitterSource = new TwitterFilterSource(
+				TwitterFilterSourceExample.class.getResource(propertiesPath)
+						.getFile());
+		*/
+		TwitterFilterSource twitterSource = new TwitterFilterSource(propertiesPath);
+		twitterSource.trackTerm("agile");
+		twitterSource.filterLanguage("fr");
 
 		DataStream<String> streamSource = env.addSource(twitterSource).flatMap(
 				new JSONParseFlatMap<String, String>() {
@@ -52,17 +93,67 @@ public class TwitterFilterSourceExample {
 					@Override
 					public void flatMap(String s, Collector<String> c)
 							throws Exception {
-						c.collect(s);
+						String text="unitialized text" ,name ="unitialized name";
+						try {
+							text = this.getString(s, "text");
+						} catch (Exception e) {
+                            if (LOG.isErrorEnabled()) {
+                                LOG.error("Fail to collect text");
+                            }
+							System.err.println("Fail to collect text")	;
+						}
+						try {
+							name = this.getString(s, "user.name");
+							c.collect("name :" + name + "text :" + text);
+						} catch (Exception e) {
+                            if (LOG.isErrorEnabled()) {
+                                LOG.error("Fail to collect name");
+                            }
+							System.err.println("Fail to collect name")	;
+						}
+                        c.collect("name :" + name + "text :" + text);
+						//c.collect(s);
 					}
 				});
 
-		streamSource.print();
-
+		//streamSource.print();
+		streamSource.writeAsText(outputPath);
 		try {
+            if (LOG.isInfoEnabled()) {
+                LOG.info("Twitter Streaming API tracking AGILE in progress");
+            }
 			env.execute("Twitter Streaming Test");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
+	private static boolean fileInput = false;
+	private static boolean fileOutput = false;
+	private static String propertiesPath;
+	private static String outputPath;
+
+	private static boolean parseParameters(String[] args) {
+		if (args.length > 0) {
+			// parse input arguments
+			fileOutput = true;
+			if (args.length == 2) {
+				fileInput = true;
+				propertiesPath = args[0];
+				outputPath = args[1];
+			} else if (args.length == 1) {
+				outputPath = args[0];
+			} else {
+				System.err.println("USAGE:\nTwitterStream [<pathToPropertiesFile>] <result path>");
+				return false;
+			}
+		} else {
+			System.out.println("Executing TwitterStream example with built-in default data.");
+			System.out.println("  Provide parameters to read input data from a file.");
+			System.out.println("  USAGE: TwitterStream [<pathToPropertiesFile>] <result path>");
+		}
+		return true;
+	}
+
 }
